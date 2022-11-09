8a80a3bf98e6d4413d8dd42f2ab56442fb3efa5c
arunshah1818 <113005667+arunshah1818@users.noreply.github.com>
2022-11-09 12:01:22 +0530

    Add files via upload

---
+++ arun.js
@@ -0,0 +1,34 @@
+// var tweet = prompt("arun is the best webdeveloper");
+// var tweetcount = tweet.length
+// alert("your tweet" + tweetcount + "charater you have" + (140 - tweetcount) + "character remain" )
+
+// slice can cut the long write
+// var tweet = prompt("write your tweet" );
+// var tweetslice = tweet.slice(0 , 120);
+// alert(tweetslice);
+
+//   var arun = prompt("what is your name");
+//  var uppercase = arun.slice(0,1);
+//  var firstname = uppercase.toUpperCase();
+//   var lastletter= arun.slice(1, arun.length);
+//  var writename =  firstname + lastletter;
+//  alert("Hello" + " "+ arun.toUpperCase());
+
+// enter your dog name 
+// var dogage = prompt("how many old your dog");
+// var humanage = ((dogage - 2) *4) + 21;
+// alert("your dog human age is" +" "+ humanage );
+
+//  prompt("Enter your name");
+//  prompt("Enter your partner name");
+
+// var lovescore = Math.random() *100;
+// lovescore = Math.floor(lovescore) + 1;
+// alert("your love score is"+ lovescore + "%");
+
+
+
+
+
+
+
